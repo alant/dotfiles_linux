8b3d4966309ad7bb245cf98ab6c4eaf5fcfba56a
Ubuntu <ubuntu@ip-10-248-81-88.us-west-2.compute.internal>
2014-04-29 16:26:39 +0000

    added javascript, auto-complete, python

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
 ;; -*- mode: emacs-lisp -*-
 ;; Simple .emacs configuration
-
 ;; ---------------------
 ;; -- Global Settings --
 ;; ---------------------
@@ -75,3 +74,52 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;;
+;;alan added section
+;;
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+; Use dictionaries by default
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
+
+(add-to-list 'load-path "~/.emacs.d/yasnippet")
+(require 'yasnippet)
+(yas--initialize)
+;; Load the snippet files themselves
+(yas/load-directory "~/.emacs.d/yasnippet/snippets/text-mode")
+;; Let's have snippets in the auto-complete dropdown
+(add-to-list 'ac-sources 'ac-source-yasnippet)
+
+;;js2-mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;;
+;;python specific stuff
+;;
+; python-mode
+(setq py-install-directory "~/.emacs.d/python-mode-6.1.3")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
